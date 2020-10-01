e9a2d4545c0e3a65660ffbac3173fcfa57869d75
hansen sudarma halim <hansensudarmahalim@gmail.com>
2020-10-01 23:24:42 +0700

    trivia

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,26 @@
 const router = require('express').Router()
-
+const axios = require('axios')
 const userRoute = require('./userRoute')
 const animeRoute = require('./animeRoute')
 
 // router.use('/animes', animeRoute)
 router.use('/users', userRoute)
+
+//api trivia
+router.get("/trivia", (req, res, next) => {
+
+    axios({
+        method: "post",
+        url: "https://opentdb.com/api.php?amount=4&category=31&type=boolean"
+    })
+        .then(response => {
+            let trivia = response.data
+            res.status(200).json({
+                trivia
+            })
+        })
+        .catch(err => next(err))
+})
 
 
 module.exports = router
