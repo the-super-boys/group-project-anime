c20f6d62f816edd3e366a228d815a3530ff25f16
hansen sudarma halim <hansensudarmahalim@gmail.com>
2020-10-02 00:42:30 +0700

    resolve

--- client/resources/js/main.js
+++ client/resources/js/main.js
@@ -1,14 +1,14 @@
 
 let baseUrl = `http://localhost:3000`
 
-$( document ).ready(function() {
+$(document).ready(function () {
   // Handler for .ready() called.
   console.log("Hello world")
 });
 
 function register(event) {
   event.preventDefault()
-  $( "#error-alert-register" ).empty()
+  $("#error-alert-register").empty()
   let newUserObj = {
     first_name: $("#first_name-register").val(),
     last_name: $("#last_name-register").val(),
@@ -16,20 +16,20 @@ function register(event) {
     password: $("#password-register").val(),
   }
   $.ajax({
-    method:"POST",
+    method: "POST",
     url: `${baseUrl}/users/register`,
     data: newUserObj
   })
     .done(result => {
       $("#first_name-register").val(""),
-      $("#last_name-register").val(""),
-      $("#email-register").val(""),
-      $("#password-register").val(""),
-      console.log(result,">>>>>>REGISTER SUCCESS")
+        $("#last_name-register").val(""),
+        $("#email-register").val(""),
+        $("#password-register").val(""),
+        console.log(result, ">>>>>>REGISTER SUCCESS")
     })
     .fail(error => {
-      $.each(error.responseJSON.errors, function( key, value ) {
-        $( "#error-alert-register" ).append( `
+      $.each(error.responseJSON.errors, function (key, value) {
+        $("#error-alert-register").append(`
         <div class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>Errors!</strong> ${value}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -41,7 +41,6 @@ function register(event) {
       console.log(error, ">>>>ERROR REGISTER")
     })
 }
-=======
 var baseUrl = 'http://localhost:3452';
 var params = null;
 var isLoading = null;
