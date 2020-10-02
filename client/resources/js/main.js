f880f29bf862bd732b3fbb674a8e1ded03b4d6f9
hansen sudarma halim <hansensudarmahalim@gmail.com>
2020-10-02 10:44:12 +0700

    sudah

--- client/resources/js/main.js
+++ client/resources/js/main.js
@@ -1,9 +1,68 @@
 let baseUrl = `http://localhost:3452`
 var params = null;
-var isLoading = null;
-var error = null;
-var movie = null;
 
+var IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+var POSTER_SIZE = 'w500';
+
+let movies = { movies: [] };
+let isLoading;
+let searchTerm = '';
+let error;
+
+//setup before functions
+var typingTimer; //timer identifier
+var doneTypingInterval = 3000; //time in ms, 5 second for example
+var $input = $('#search-widget');
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
+//user is "finished typing," do something
+function doneTyping() {
+  searchTerm = $('#search-widget').val();
+  $('#movies-container').empty();
+  alert(searchTerm);
+  handleFetchMovies(false, searchTerm);
+}
+
+function initMovie() {
+  const sessionMovies = window.sessionStorage.getItem('anime_movies');
+  if (sessionMovies) {
+    console.log('Grabbing from sessionStorage.');
+    curr_movies = JSON.parse(sessionMovies);
+    console.log(curr_movies.movies);
+    let moviesresult = curr_movies.movies;
+    console.log(moviesresult.length);
+    let output = '';
+    $.each(moviesresult, function (key, val) {
+      let image = `${val.poster_path}` && `${IMAGE_BASE_URL}${POSTER_SIZE}${val.poster_path}`;
+      console.log(image);
+      let movieId = val.id;
+      let href = movieId ? `/movies/${movieId}` : '1';
+      let src = image ? image : `./resources/img/no_image.jpg`;
+      output = `
+          <div class="wrapper">
+            <a href="${href}">
+              <img class="clickable" src="${src}" alt="moviethumb" />
+            </a>
+          </div>
+        `;
+      $('#movies-container').append(output);
+    });
+  } else {
+    console.log('Grabbing from API.');
+    searchTerm = 'Anime';
+    handleFetchMovies(false, searchTerm);
+  }
+}
 
 $(document).ready(function () {
   // Handler for .ready() called.
@@ -42,6 +101,7 @@ function afterLogin() {
   $("#login-page").hide()
   $("#register-page").hide()
   $("#home-page").show()
+  initMovie() // ini untuk inisialisasi movies pada saat masuk homepage
   $.ajax({
     method: "get",
     url: `${baseUrl}/trivia`
@@ -60,6 +120,7 @@ function afterLogin() {
         </div>
         ` );
       $("#trivia-page").show()
+
     })
     .fail(err => {
       console.log(err.responseJSON.errors, ">>>>ERROR REGISTER")
@@ -127,29 +188,90 @@ const handleFetchMovie = async (event) => {
   }
 };
 
+const handleFetchMovies = async (loadMore, searchTerm) => {
+  try {
+    isLoading = true;
+    error = false;
+    $.ajax({
+      url: `${baseUrl}/movies/`,
+      method: 'POST',
+      headers: {
+        token: localStorage.getItem("token")
+      },
+      data: {
+        movies,
+        loadMore,
+        searchTerm,
+      },
+    })
+      .done((res) => {
+        movies.currentPage = res.anime_movies.currentPage;
+        movies.heroImage = res.anime_movies.heroImage;
+        movies.totalPages = res.anime_movies.totalPages;
+        movies.movies = [];
+
+        res.anime_movies.movies.map((movie) => {
+          movies.movies.push(movie);
+        });
+
+        console.log(movies);
+
+        console.log(movies.movies.length);
+
+        if (movies.movies.length > 0) {
+          window.sessionStorage.setItem('anime_movies', JSON.stringify(movies));
+        }
+
+        let output = '';
+        $.each(movies.movies, function (key, val) {
+          let image = `${val.poster_path}` && `${IMAGE_BASE_URL}${POSTER_SIZE}${val.poster_path}`;
+          let movieId = val.id;
+          let href = movieId ? `/movies/${movieId}` : '1';
+          let src = image ? image : `./resources/img/no_image.jpg`;
+          output = `
+            <div class="wrapper">
+              <a href="${href}">
+                <img class="clickable" src="${src}" alt="moviethumb" />
+              </a>
+            </div>
+          `;
+          $('#movies-container').append(output);
+        });
+      })
+      .fail((err) => {
+        console.log(err, '<<<<<<<<<<<<<<<<< ERROR');
+      });
+  } catch (err) {
+    error = true;
+  }
+  isLoading = false;
+};
+
+const handleLoadMore = () => handleFetchMovies(true, searchTerm);
+
 function login(event) {
   event.preventDefault();
   let email = $("#login-email").val();
   let password = $("#login-password").val();
   $.ajax({
-          url: `${baseUrl}/users/login`,
-          method: "POST",
-          data: {
-              email,
-              password
-          }
-      })
-      .done(data => {
-          console.log(data, '<<<<<<<<<<<<<<<<< data login');
-          localStorage.setItem("token", data.token)
-          $("#login-email").val("");
-          $("#login-password").val("");
-          afterLogin()
+    url: `${baseUrl}/users/login`,
+    method: "POST",
+    data: {
+      email,
+      password
+    }
+  })
+    .done(data => {
+      console.log(data, '<<<<<<<<<<<<<<<<< data login');
+      localStorage.setItem("token", data.token)
+      $("#login-email").val("");
+      $("#login-password").val("");
+      afterLogin()
 
-      })
-      .fail(err => {
-          console.log(err.responseJSON.errors, '<<<<<<<<<<<<<<<<< error login');
-      })
+    })
+    .fail(err => {
+      console.log(err.responseJSON.errors, '<<<<<<<<<<<<<<<<< error login');
+    })
 }
 
 function logout(event) {
@@ -166,7 +288,7 @@ function logout(event) {
 function onSignIn(googleUser) {
   var tokenGoogle = googleUser.getAuthResponse().id_token;
   $.ajax({
-    url: baseUrl+ "/users/googlesign",
+    url: baseUrl + "/users/googlesign",
     method: "POST",
     data: {
       tokenGoogle
